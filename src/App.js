d7d7ef8fde043960d89b6c5e448f7d94e124cd29
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-20 07:03:38 +0200

    adding the state with name property

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React, { Component } from "react";
 import Header from "./components/header";
 
 class App extends Component {
+  state = {
+    name: "Abdelrahman Nasser",
+  };
+
   render() {
     return (
       <React.Fragment>
-        <Header></Header>
+        <Header name={this.state.name}></Header>
       </React.Fragment>
     );
   }
