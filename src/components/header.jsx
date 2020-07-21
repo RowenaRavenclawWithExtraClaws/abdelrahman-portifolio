7d6bdf02103c77eb228c59f9a7fccee64ea76969
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-21 07:02:43 +0200

    add extra content

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,17 @@ import Photo from "../static/Photo.png";
 
 class Header extends Component {
   render() {
+    const info = this.props.info;
+
     return (
       <header>
         <div className="header">
           <img src={Photo} alt="logo" />
-          <h1> {this.props.name}</h1>
+          <div className="col">
+            <h1>{info.name}</h1>
+            <h3>{info.profession}</h3>
+            <p>{info.about}</p>
+          </div>
         </div>
       </header>
     );
