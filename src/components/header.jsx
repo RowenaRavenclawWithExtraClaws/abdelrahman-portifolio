ef17a1d10e354fb435af9f8af7ec74e94a4d0b41
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-21 07:41:36 +0200

    adding onClick event and a title to the github logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,7 +10,13 @@ class Header extends Component {
       <header>
         <div className="header">
           <img src={Photo} className="photo" alt="logo" />
-          <img src={Octocat} className="github" alt="logo" />
+          <img
+            src={Octocat}
+            title="Github profile"
+            className="github"
+            alt="logo"
+            onClick={() => window.open(info.github)}
+          />
           <div className="col">
             <h1>{info.name}</h1>
             <h3>{info.profession}</h3>
