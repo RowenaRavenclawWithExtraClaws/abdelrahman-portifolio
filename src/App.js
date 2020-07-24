7cdd921cd95de9bccc9ef85db715b8ecc03c9f78
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-25 00:29:39 +0200

    adding more state props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Photo from "./static/Photo.png";
+import Octocat from "./static/Octocat.png";
 import Header from "./components/header";
 import Heroics from "./components/heroics";
 
@@ -8,13 +10,15 @@ class App extends Component {
     profession: "Softwre Engineer",
     about: "A dude who writes code for a living!",
     github: "https://github.com/RowenaRavenclawWithExtraClaws",
+    photo: Photo,
+    octocat: Octocat,
   };
 
   render() {
     return (
       <React.Fragment>
         <Header info={this.state}></Header>
-        <Heroics>Ahmed</Heroics>
+        <Heroics></Heroics>
       </React.Fragment>
     );
   }
