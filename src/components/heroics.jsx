cec5f0ea1ccbf7f6c240fee8ff481340984f3ab5
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-25 00:36:53 +0200

    removing the state

--- src/components/heroics.jsx
+++ src/components/heroics.jsx
@@ -1,39 +1,14 @@
 import React, { Component } from "react";
 import Project from "./project";
-import Tincan from "../static/tincan.png";
-import Reddit from "../static/redditApi.png";
-import Fluid from "../static/fluidUI.png";
 
 class Heroics extends Component {
-  state = [
-    {
-      projectName: "Tincan",
-      image: Tincan,
-      cssClass: "project1",
-      link: "https://github.com/RowenaRavenclawWithExtraClaws/TincanApp",
-    },
-    {
-      projectName: "Reddit API",
-      image: Reddit,
-      cssClass: "project2",
-      link:
-        "https://github.com/RowenaRavenclawWithExtraClaws/Reddit-like-GraphQL-API",
-    },
-    {
-      projectName: "Fluid UI",
-      image: Fluid,
-      cssClass: "project3",
-      link: "https://git.divvoice.eu/internship/nasser/-/tree/master",
-    },
-  ];
-
   render() {
-    let info = this.state;
+    let projects = this.props.projects;
 
     return (
       <div className="heroic">
         <h1>Projects</h1>
-        {info.map((project) => (
+        {projects.map((project) => (
           <Project info={project}></Project>
         ))}
       </div>
