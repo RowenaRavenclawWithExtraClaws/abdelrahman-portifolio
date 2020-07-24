80e7d9c925c86190ece6db419c2455346bd17747
RowenaRavenclawWithExtraClaws <abdelrahmannasser48@gmail.com>
2020-07-24 09:00:04 +0200

    using the achievement component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/header";
+import Heroics from "./components/heroics";
 
 class App extends Component {
   state = {
@@ -13,6 +14,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header info={this.state}></Header>
+        <Heroics>Ahmed</Heroics>
       </React.Fragment>
     );
   }
